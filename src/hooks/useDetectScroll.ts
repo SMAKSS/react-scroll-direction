778da084dd0889e5e2eb78dca1375be0d94b4a5e
MAKSS <kazemi914@live.com>
2023-05-19 23:59:12 +0330

    [ts-migrate][src] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/hooks/useDetectScroll.ts
+++ src/hooks/useDetectScroll.ts
@@ -1,7 +1,9 @@
+// @ts-expect-error TS(2307): Cannot find module 'react' or its corresponding ty... Remove this comment to see the full error message
 import { useState, useEffect } from "react";
+// @ts-expect-error TS(7016): Could not find a declaration file for module 'prop... Remove this comment to see the full error message
 import PropTypes from "prop-types";
 
-function useDetectScroll(props) {
+function useDetectScroll(props: any) {
   const {
     thr = 0,
     axis = "y",
@@ -14,7 +16,7 @@ function useDetectScroll(props) {
   useEffect(() => {
     const threshold = thr > 0 ? thr : 0;
     let ticking = false;
-    let lastScroll = undefined;
+    let lastScroll: any = undefined;
 
     axis === "y"
       ? (lastScroll = window.pageYOffset)
