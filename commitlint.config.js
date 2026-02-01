4239e348025a58f709c84d5fc89163f899517851
SMAKSS <kazemi914@live.com>
2026-02-01 04:14:25 +0000

    fix(config): remove 'test' type from commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -42,7 +42,6 @@ export default {
                 'revert', // Revert previous commit
                 'wip', // Work in progress
                 'hotfix', // Critical fixes
-                'test', // Tests
             ],
         ],
 
